c65aed15ce3c0e5e9e1e55fb63c2892128cfbc9c
adarsh <adarshprakash16@gmail.com>
2025-05-27 10:16:01 +0530

    initializing spring project

---
+++ src/main/java/com/adarsh/resumedS3/ResumedS3Application.java
@@ -0,0 +1,13 @@
+package com.adarsh.resumedS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumedS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumedS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/adarsh/resumedS3/ResumedS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarsh.resumedS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumedS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
